fb746f34ebb7ac02659d30482c9d974a8a096803
filipboric <filipmcsg@gmail.com>
2019-11-10 16:21:44 +0100

    Interfaces for services added

---
+++ MonoProject/MonoProject.Service/Services.Common/IVehicleMakeService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoProject.Service.Services.Common
+{
+    public interface IVehicleMakeService
+    {
+        void AddVehicleMake(VehicleMakeEntity vehicleMake);
+        VehicleMakeEntity GetVehicleMake(int id);
+        List<VehicleMakeEntity> GetVehicleMakes(string search, string sortOrder);
+        void UpdateVehicleMake(VehicleMakeEntity UpdateVehicleMake);
+        void DeleteVehicleMake(VehicleMakeEntity vehicleMakeDelete);
+    }
+}
---
+++ MonoProject/MonoProject.Service/Services.Common/IVehicleModelService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoProject.Service.Services.Common
+{
+    public interface IVehicleModelService
+    {
+        void AddVehicleModel(VehicleModelEntity vehicleModel);
+        VehicleModelEntity GetVehicleModel(int id);
+        List<VehicleModelEntity> GetVehicleModels(string search, string sortOrder);
+        void UpdateVehicleModel(VehicleModelEntity updateVehicleModel);
+        void DeleteVehicleModel(VehicleModelEntity vehicleModelDelete);
+    }
+}
