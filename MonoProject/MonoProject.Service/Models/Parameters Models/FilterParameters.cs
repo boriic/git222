2866fef46fe32f013291c5bc138ff9db098528ad
filipboric <filipmcsg@gmail.com>
2019-11-10 16:38:23 +0100

    Parameters Added

---
+++ MonoProject/MonoProject.Service/Models/Parameters Models/FilterParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoProject.Service.Models.Parameters_Models
+{
+    public class FilterParameters
+    {
+        public string Search { get; set; }
+        public int MakeId { get; set; }
+    }
+}
---
+++ MonoProject/MonoProject.Service/Models/Parameters Models/PageParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoProject.Service.Models.Parameters_Models
+{
+    public class PageParameters
+    {
+        public int PageSize { get; set; }
+        public int Page { get; set; }
+    }
+}
